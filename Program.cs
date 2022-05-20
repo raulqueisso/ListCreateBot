cf47af2a6164ddd9a4800060dafa134009d45ab2
Raul Felix <raulfelixsousa@gmail.com>
2022-05-20 20:38:29 -0300

    Make bot repeat user message
    Code copied from https://telegrambots.github.io/book/1/example-bot.html

--- Program.cs
+++ Program.cs
@@ -1,18 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace ListCreateBot {
     class Program {
-        static void Main(string[] args) {
+        static async Task Main(string[] args) {
             var botToken = GetBotToken();
+            var botClient = new TelegramBotClient(botToken);
 
-            Console.WriteLine(botToken);
+            using var cts = new CancellationTokenSource();
 
+            StartBot(botClient, cts);
+
+            var me = await botClient.GetMeAsync();
+            Console.WriteLine($"Start listening for @{me.Username}");
             Console.ReadLine();
+
+            // Send cancellation request to stop bot
+            cts.Cancel();
+        }
+
+        private static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken) {
+            // Only process Message updates: https://core.telegram.org/bots/api#message
+            if (update.Type != UpdateType.Message) {
+                return;
+            }
+            // Only process text messages
+            if (update.Message.Type != MessageType.Text) {
+                return;
+            }
+
+            var chatId = update.Message.Chat.Id;
+            var messageText = update.Message.Text;
+
+            Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+            // Echo received message text
+            Message sentMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "You said:\n" + messageText,
+                cancellationToken: cancellationToken);
+        }
+
+        private static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken) {
+            var ErrorMessage = exception switch {
+                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
         }
 
         private static string GetBotToken() {
             DotNetEnv.Env.TraversePath().Load();
             return Environment.GetEnvironmentVariable("BOT_TOKEN");
         }
+
+        private static void StartBot(TelegramBotClient botClient, CancellationTokenSource cts) {
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            var receiverOptions = new ReceiverOptions {
+                AllowedUpdates = Array.Empty<UpdateType>() // receives all update types
+            };
+            botClient.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                errorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token);
+        }
+
     }
 }
