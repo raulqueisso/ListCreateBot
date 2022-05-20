1da9997f66d8062dd241d332e009f0e42ea21e49
Raul Felix <raulfelixsousa@gmail.com>
2022-05-19 21:41:03 -0300

    Get bot token from .env file

--- Program.cs
+++ Program.cs
@@ -3,7 +3,16 @@
 namespace ListCreateBot {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var botToken = GetBotToken();
+
+            Console.WriteLine(botToken);
+
+            Console.ReadLine();
+        }
+
+        private static string GetBotToken() {
+            DotNetEnv.Env.TraversePath().Load();
+            return Environment.GetEnvironmentVariable("BOT_TOKEN");
         }
     }
 }
