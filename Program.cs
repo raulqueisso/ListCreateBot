4abaa336c46eeb5d1a6a5869b0ce9550ffcfb8ef
Raul Felix <raulfelixsousa@gmail.com>
2022-05-26 21:13:58 -0300

    Find chat id in the JSON file

--- Program.cs
+++ Program.cs
@@ -68,7 +68,14 @@ namespace ListCreateBot {
 
                 WriteUpdate(chatId, "/add", null);
             } else {
-                text = "Sorry, I can't understand what you are trying to do. Use my commands, please.";
+                var botUpdatesId = GetBotUpdatesId(chatId);
+
+                if (botUpdatesId != -1 && botUpdates[botUpdatesId].waitedInput != null) {
+                    text = $"{messageText} added to the list.";
+                }
+                else {
+                    text = "Sorry, I can't understand what you are trying to do. Use my commands, please.";
+                }
             }
 
             SendMessage(botClient, cancellationToken, chatId, text);
@@ -120,6 +127,20 @@ namespace ListCreateBot {
             var botUpdateString = JsonConvert.SerializeObject(botUpdates);
 
             System.IO.File.WriteAllText(fileName, botUpdateString);
+        }
+
+        private static int GetBotUpdatesId(long chatId) {
+            int botUpdatesId = -1;
+
+            var botUpdatesLength = botUpdates.Count();
+
+            for (int i = 0; i < botUpdatesLength; i++) {
+                if (botUpdates[i].chatId == chatId) {
+                    botUpdatesId = i;
+                }
+            }
+
+            return botUpdatesId;
         }
     }
 }
